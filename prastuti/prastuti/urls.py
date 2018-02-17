3edff24fa7f65591d7b9b6131975fcad3b36d863
verma-varsha <varsha.verma.eee15@itbhu.ac.in>
2018-02-17 10:43:26 +0530

    homepage frontend

--- prastuti/prastuti/urls.py
+++ prastuti/prastuti/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from website.views import(
+    index,
+)
 
 urlpatterns = [
+    url(r'^$',index, name='index'),
     path('admin/', admin.site.urls),
 ]
